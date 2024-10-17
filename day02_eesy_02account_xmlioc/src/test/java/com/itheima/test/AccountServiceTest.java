4b5ab03ea83adc98e93689c29ac65af0d1312743
twu <jasonxts19@163.com>
2024-10-17 17:03:22 +0800

    update

--- day02_eesy_02account_xmlioc/src/test/java/com/itheima/test/AccountServiceTest.java
+++ day02_eesy_02account_xmlioc/src/test/java/com/itheima/test/AccountServiceTest.java
@@ -23,18 +23,37 @@ public class AccountServiceTest {
 
     @Test
     public void testFindOne() {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("bean2.xml");
+        IAccountService as = ac.getBean("accountService",IAccountService.class);
+        Account account = as.findAccountById(1);
+        System.out.println(account);
     }
 
     @Test
     public void testSave() {
+        Account account = new Account();
+        account.setName("test");
+        account.setMoney(12345f);
+        ApplicationContext ac = new ClassPathXmlApplicationContext("bean2.xml");
+        IAccountService as = ac.getBean("accountService",IAccountService.class);
+        as.saveAccount(account);
     }
 
     @Test
     public void testUpdate() {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("bean2.xml");
+        IAccountService as = ac.getBean("accountService",IAccountService.class);
+        Account account = as.findAccountById(4);
+        account.setMoney(23456f);
+        as.updateAccount(account);
     }
 
     @Test
     public void testDelete() {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("bean2.xml");
+        IAccountService as = ac.getBean("accountService",IAccountService.class);
+        Account account = as.findAccountById(4);
+        as.deleteAccount(4);
     }
 
 }
